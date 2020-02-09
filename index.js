cc437d0c18cbce4c6c79c78a01ef9c482cdacbf0
Recep Öztürk <turkishteacher23@gmail.com>
2020-02-09 17:23:03 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -41,7 +41,7 @@ let [black_and_white,, black, pink,] =farmAnimals.split(" ")
 // 4. Use destructuring to assign appropriate variables using the color names.
 let [red, orange, yellow, green, blue, indigo, violet]=colors
 // 5. Some people have a really hard time picking out indigo, so let's leave that one out, using the first letter of each color as the variable names.
-
+let [red, orange, yellow, green, blue, , violet]= colors
 // 6. But wait! Indigo is now feeling *super* left out. Let's only assign indigo using indg. 
 
 // Objects
